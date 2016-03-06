e1a1911ac01a993e6f5a0637bc1d91ba3296ef4d
shaneo <shaneo@shaneo.(none)>
2016-03-06 17:46:24 -0500

    Schools mods plus original CSV

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ var path = require("path");
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
-
-var CONTACTS_COLLECTION = "contacts";
+var MONGOLAB_URI = "mongodb://wcpss:WCP55sch00l5@ds023478.mlab.com:23478/heroku_qgqdjjlx";
+var PORT = 80;
+var SCHOOLS_COLLECTION = "schools";
 
 var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
+var db;  // Database connection
 
-// Connect to the database before starting the application server. 
-mongodb.MongoClient.connect(process.env.MONGOLAB_URI, function (err, database) {
+// Connect to the database  
+mongodb.MongoClient.connect(MONGOLAB_URI, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
@@ -25,13 +25,13 @@ mongodb.MongoClient.connect(process.env.MONGOLAB_URI, function (err, database) {
   console.log("Database connection ready");
 
   // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
+  var server = app.listen(PORT || 8080, function () {
     var port = server.address().port;
-    console.log("App now running on port", port);
+    console.log("App now running on port ", port);
   });
 });
 
-// CONTACTS API ROUTES BELOW
+// SCHOOLS API ROUTES BELOW
 
 // Generic error handler used by all endpoints.
 function handleError(reason, message, code) {
@@ -39,72 +39,31 @@ function handleError(reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
-/*  "/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
+/*  "/schools"
+ *    GET: finds all schools
  */
 
-app.get("/contacts", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+app.get("/schools", function(req, res) {
+  db.collection(SCHOOLS_COLLECTION).find({}).toArray(function(err, docs) {
     if (err) {
-      handleError(err.message, "Failed to get contacts.");
+      handleError(err.message, "Failed to get schools.");
     } else {
       res.status(200).json(docs);  
     }
   });
 });
 
-app.post("/contacts", function(req, res) {
-  var newContact = req.body;
-  newContact.createDate = new Date();
-
-  if (!(req.body.firstName || req.body.lastName)) {
-    handleError("Invalid user input", "Must provide a first or last name.", 400);
-  }
-
-  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
-    if (err) {
-      handleError(err.message, "Failed to create new contact.");
-    } else {
-      res.status(201).json(doc.ops[0]);
-    }
-  });
-});
-
 /*  "/contacts/:id"
- *    GET: find contact by id
- *    PUT: update contact by id
- *    DELETE: deletes contact by id
+ *    GET: find school by id
  */
 
-app.get("/contacts/:id", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+app.get("/schools/:leaid", function(req, res) {
+  db.collection(SCHOOLS_COLLECTION).findOne({ leaid: req.params.id }, function(err, doc) {
     if (err) {
-      handleError(err.message, "Failed to get contact");
+      handleError(err.message, "Failed to get school");
     } else {
       res.status(200).json(doc);  
     }
   });
 });
 
-app.put("/contacts/:id", function(req, res) {
-  var updateDoc = req.body;
-
-  db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
-    if (err) {
-      handleError(err.message, "Failed to update contact");
-    } else {
-      res.status(204).end();
-    }
-  });
-});
-
-app.delete("/contacts/:id", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
-    if (err) {
-      handleError(err.message, "Failed to delete contact");
-    } else {
-      res.status(204).end();
-    }
-  });
-});
