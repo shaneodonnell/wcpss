70264399e3b3f6092634cc1ce1e04dcdfd2beb23
Shane O'Donnell <shaneo@knownormal.com>
2016-03-06 20:53:33 -0500

    Correcting angular app name

--- server.js
+++ server.js
@@ -39,10 +39,6 @@ function handleError(reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
-app.get("/", function(req, res) {
-	handleError(err.message, "Move along.  Nothing to see here.");
-});
-
 /*  "/schools"
  *    GET: finds all schools
  */
