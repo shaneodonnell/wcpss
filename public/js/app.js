64752be6de6bd53e6c2856e946240632b02f9c97
Shane O'Donnell <shaneo@knownormal.com>
2016-03-11 12:50:30 -0500

    Adding back contact stuff to controller

--- public/js/app.js
+++ public/js/app.js
@@ -39,4 +39,45 @@ angular.module("schoolsApp", ['ngRoute'])
     })
     .controller("ListController", function(schools, $scope) {
         $scope.schools = schools.data;
+    })
+    .controller("NewSchoolController", function($scope, $location, Schools) {
+        $scope.back = function() {
+            $location.path("#/");
+        }
+
+        $scope.saveSchool = function(school) {
+            Schools.createSchool(school).then(function(doc) {
+                var schoolUrl = "/school/" + doc.data._id;
+                $location.path(schoolUrl);
+            }, function(response) {
+                alert(response);
+            });
+        }
+    })
+    .controller("EditSchoolController", function($scope, $routeParams, Schools) {
+        Schools.getSchool($routeParams.schoolId).then(function(doc) {
+            $scope.school = doc.data;
+        }, function(response) {
+            alert(response);
+        });
+
+        $scope.toggleEdit = function() {
+            $scope.editMode = true;
+            $scope.schoolFormUrl = "school-form.html";
+        }
+
+        $scope.back = function() {
+            $scope.editMode = false;
+            $scope.schoolFormUrl = "";
+        }
+
+        $scope.saveSchool = function(school) {
+            Schools.editSchool(school);
+            $scope.editMode = false;
+            $scope.schoolFormUrl = "";
+        }
+
+        $scope.deleteSchool = function(schoolId) {
+            Schools.deleteSchool(schoolId);
+        }
     });
